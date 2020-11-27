3fc4522c83e0fe9ad61251b1d95da6a71363519e
Carlos Rodríguez <4779458@alu.murcieduca.es>
2020-11-27 10:07:48 +0100

    Controladores, filtros y formularios

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
-    Vista detalle producto {{$id}}
+<div class="row">
 
+    <div class="col-sm-4">
+        <img src="https://picsum.photos/200/300/?random" style="height:200px" />
+    </div>
+    <div class="col-sm-8">
+        <p>Nombre: {{$producto[0]}}</p>
+        <p>Categoría: {{$producto[1]}}</p>
+        <p>Estado: Producto actualmente comprado</p>
+        <a href="#" class="btn btn-danger">Pendiente de compra</a>
+        <a href="{{action('App\Http\Controllers\ProductoController@getEdit',[$id])}}" class="btn btn-info">Editar</a>
+        <a href="{{action('App\Http\Controllers\HomeController@getHome')}}" class="btn btn-info">Volver al index</a>
+    </div>
+</div>
 @stop
